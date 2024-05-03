445df5d20dce81890f130b23b673c066444d8f8c
ABetancourt03 <neutralzgamer@gmail.com>
2024-05-03 11:32:10 -0400

    Center header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header () {
   return (
     <>
-      <header className='flex w-screen items-center h-20 justify-between bg-[Canvas] p-10'>
+      <header className='flex w-screen items-center h-20 justify-center bg-[Canvas] p-10'>
         <h2 className='text-2xl'>📚 Reading List</h2>
       </header>
     </>
