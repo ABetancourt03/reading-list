34a6503dcdc3cc424cb1222cdf20083df08f6794
ABetancourt03 <neutralzgamer@gmail.com>
2024-05-03 11:30:41 -0400

    Change header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
-
 export default function Header () {
   return (
     <>
-      <header className='flex w-screen items-center h-20 justify-between bg-[#DEDEDE] p-10'>
+      <header className='flex w-screen items-center h-20 justify-between bg-[Canvas] p-10'>
         <h2 className='text-2xl'>📚 Reading List</h2>
       </header>
     </>
