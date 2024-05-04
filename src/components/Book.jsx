99607478e6569dfd77810ed1f69ac758ffb1f89a
ABetancourt03 <neutralzgamer@gmail.com>
2024-05-04 15:19:17 -0400

    Use destructuration

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,14 +1,16 @@
 export default function Book ({ book }) {
+  const { title, genre, cover } = book
+
   return (
     <article className='flex h-[400px] w-[280px] text-xl font-medium'>
-      <img className='h-full w-full' src={book.cover} alt={book.title} />
+      <img className='h-full w-full' src={cover} alt={title} />
       <a
         href='/'
         className='book-info absolute text-white flex h-[400px] w-[280px] flex-col justify-around transition hover:bg-[#00000090]'
       >
         <div>
-          <p>Titulo: {book.title}</p>
-          <p>Género: {book.genre}</p>
+          <p>Titulo: {title}</p>
+          <p>Género: {genre}</p>
         </div>
 
         <p>Ver más detalles</p>
